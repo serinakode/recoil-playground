0b4f63946400effdd136983c160ab55d2f2bb2b5
Serina <serina.ko@myob.com>
2020-07-14 18:52:24 +1000

    create the AppWithRecoil component

---
+++ src/AppWithRecoil.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import { atom, RecoilRoot, useRecoilState } from 'recoil';
+
+export const Qna = atom({
+  key: 'QNA',
+  default: 'When is the next public holiday in Victoria?'
+})
+
+function AppWithRecoil() {
+
+  const [qnaState, setQnaState] = useRecoilState(Qna)
+  return (
+    <div>
+      <h1>
+        Welcome to the App with Recoil: {qnaState}
+      </h1>
+      <button onClick={() => setQnaState('Friday before the AFL Grand Final 🤔')}>View Answer</button>
+    </div>
+  );
+}
+
+export default function AppWithRecoilRoot() {
+  return (
+    <RecoilRoot>
+      <AppWithRecoil />
+    </RecoilRoot>
+  )
+}
