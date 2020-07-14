04b64796081623cbf6539a89ce70051b241d465b
Serina <serina.ko@myob.com>
2020-07-14 18:46:56 +1000

    create the AppWithRedux component

---
+++ src/AppWithRedux.jsx
@@ -0,0 +1,44 @@
+import React from 'react'
+import { createStore } from 'redux';
+import { Provider, connect } from 'react-redux'
+
+const answerReducer = (state, action) => {
+  switch (action.type) {
+  case 'SHOW_ANSWER':
+    return action.payload
+  default:
+    return state
+  }
+}
+
+const store = createStore(answerReducer, 'What goes up, but never comes down?')
+
+const mapStateToProps = (state) => {
+  return {
+    question: state
+  }
+
+}
+const mapDispatchToProps = (dispatch) => ({
+  showAnswer: (value) => dispatch({ type: "SHOW_ANSWER", payload: value })
+})
+
+function AppWithRedux(props) {
+  return (
+    <div>
+      <h1>Welcome to the APP with redux :{props.question}</h1>
+      <button onClick={() => props.showAnswer('Your age 😨')}>View Answer</button>
+    </div>
+  )
+}
+
+const ConnectAppWithRedux = connect(mapStateToProps, mapDispatchToProps)(AppWithRedux)
+
+export default function () {
+  return (
+    <Provider store={store}>
+      <ConnectAppWithRedux />
+    </Provider>
+  )
+}
+
---
+++ src/index.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import AppWithRedux from './AppWithRedux';
+import AppWithContext from './AppWithContext';
+import AppWithRecoil from './AppWithRecoil';
+
+ReactDOM.render(
+
+  <React.StrictMode>
+    <>
+      <AppWithRedux />
+        {/*<AppWithContext/>*/}
+        {/*<AppWithRecoil/>*/}
+      </>
+  </React.StrictMode>,
+  document.getElementById('root')
+);
